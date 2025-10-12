29529acd440a35873b1512ddd427720c9b38675d
ezzatMohsen <ezzatmohsen000@gmail.com>
2025-10-12 19:02:53 +0300

    odoo customized module

--- app_odoo_customize/controllers/controllers.py
+++ app_odoo_customize/controllers/controllers.py
@@ -11,15 +11,15 @@ class AppHome(Home):
     def web_client(self, s_action=None, **kw):
         # todo: 当前只对 web，要调整为也对 website
         res = super(AppHome, self).web_client(s_action, **kw)
-
-        if kw.get('debug', False):
-            config_parameter = request.env['ir.config_parameter'].sudo()
-            app_debug_only_admin = config_parameter.get_param('app_debug_only_admin')
-            if request.session.uid and request.env.user.browse(request.session.uid)._is_admin():
-                pass
-            else:
-                if app_debug_only_admin:
-                    return request.redirect('/web/session/logout?debug=0')
+        #
+        # if kw.get('debug', False):
+        #     config_parameter = request.env['ir.config_parameter'].sudo()
+        #     # app_debug_only_admin = config_parameter.get_param('app_debug_only_admin')
+        #     if request.session.uid and request.env.user.browse(request.session.uid)._is_admin():
+        #         pass
+        #     else:
+        #         if app_debug_only_admin:
+        #             return request.redirect('/web/session/logout?debug=0')
         return res
 
 
