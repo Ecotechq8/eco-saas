026a431d5451a56b26237b49dd48a84c088bfb0b
ezzatMohsen <ezzatmohsen000@gmail.com>
2025-10-12 22:51:47 +0300

    odoo customized module

--- eco_theme/static/src/webclient/appsmenu/appsmenu.js
+++ eco_theme/static/src/webclient/appsmenu/appsmenu.js
@@ -3,72 +3,60 @@ import { url } from "@web/core/utils/urls";
 import { useBus, useService } from "@web/core/utils/hooks";
 import { registry } from "@web/core/registry";
 import { Dropdown } from "@web/core/dropdown/dropdown";
-import DatabaseManager from "web.DatabaseManager";  // OWL import
+
 
 export class AppsMenu extends Dropdown {
     setup() {
-        super.setup();
-        this.commandPaletteOpen = false;
+    	super.setup();
+    	this.commandPaletteOpen = false;
         this.commandService = useService("command");
-        this.companyService = useService('company');
-
-        if (this.companyService.currentCompany.has_background_image) {
+    	this.companyService = useService('company');
+    	if (this.companyService.currentCompany.has_background_image) {
             this.imageUrl = url('/web/image', {
                 model: 'res.company',
                 field: 'background_image',
                 id: this.companyService.currentCompany.id,
             });
-        } else {
-            this.imageUrl = '/eco_theme/static/src/img/background.png';
-        }
-
+    	} else {
+    		this.imageUrl = '/eco_theme/static/src/img/background.png';
+    	}
         useEffect(
             (isOpen) => {
-                if (isOpen) {
-                    const openMainPalette = (ev) => {
-                        if (
-                            !this.commandServiceOpen &&
-                            ev.key.length === 1 &&
-                            !ev.ctrlKey &&
-                            !ev.altKey
-                        ) {
-                            this.commandService.openMainPalette(
-                                { searchValue: `/${ev.key}` },
-                                () => { this.commandPaletteOpen = false; }
-                            );
-                            this.commandPaletteOpen = true;
-                        }
-                    };
-                    window.addEventListener("keydown", openMainPalette);
-                    return () => {
-                        window.removeEventListener("keydown", openMainPalette);
-                        this.commandPaletteOpen = false;
-                    };
-                }
+            	if (isOpen) {
+            		const openMainPalette = (ev) => {
+            	    	if (
+            	    		!this.commandServiceOpen &&
+            	    		ev.key.length === 1 &&
+            	    		!ev.ctrlKey &&
+            	    		!ev.altKey
+            	    	) {
+	            	        this.commandService.openMainPalette(
+            	        		{ searchValue: `/${ev.key}` },
+            	        		() => { this.commandPaletteOpen = false; }
+            	        	);
+	            	    	this.commandPaletteOpen = true;
+            	    	}
+            		}
+	            	window.addEventListener("keydown", openMainPalette);
+	                return () => {
+	                	window.removeEventListener("keydown", openMainPalette);
+	                	this.commandPaletteOpen = false;
+	                }
+            	}
             },
             () => [this.state.isOpen]
-        );
-
-        useBus(this.env.bus, "ACTION_MANAGER:UI-UPDATED", this.state.close);
-
-        // Remove database logo
-        useEffect(() => {
-            const logo = document.querySelector('.o_database_list img');
-            if (logo) {
-                logo.remove();
-            }
-        }, []);
+		);
+    	useBus(this.env.bus, "ACTION_MANAGER:UI-UPDATED", this.state.close);
     }
-
     onOpened() {
-        super.onOpened();
-        if (this.menuRef && this.menuRef.el) {
-            this.menuRef.el.style.backgroundImage = `url('${this.imageUrl}')`;
-        }
+		super.onOpened();
+		if (this.menuRef && this.menuRef.el) {
+			this.menuRef.el.style.backgroundImage = `url('${this.imageUrl}')`;
+		}
     }
 }
 
-// Remove unwanted user menu items
+
 const unwantedKeys = [
     "odoo_account",
     "documentation",
@@ -81,5 +69,8 @@ const unwantedKeys = [
 
 unwantedKeys.forEach(key => {
     registry.category("user_menuitems").remove(key);
-    console.log(`Removed user menu item: ${key}`);
+    console.log(key);
 });
+
+
+
