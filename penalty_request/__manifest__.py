4895bd499c4d5a17bc2138590014b299409f6c8d
ezzatMohsen <ezzatmohsen000@gmail.com>
2025-11-10 15:04:15 +0200

    hr modules

--- penalty_request/__manifest__.py
+++ penalty_request/__manifest__.py
@@ -12,7 +12,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'hr', 'eco_contract_enhancement'],
+    'depends': ['base', 'hr', 'eco_contract_enhancement','mail'],
 
     # always loaded
     'data': [
