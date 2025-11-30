25726a930458a204be4d506d043202aafc92a75b
ezzatMohsen <ezzatmohsen000@gmail.com>
2025-11-30 12:14:14 +0200

    fix hr payroll employee

--- om_hr_payroll/wizard/hr_payroll_payslips_by_employees.py
+++ om_hr_payroll/wizard/hr_payroll_payslips_by_employees.py
@@ -1,7 +1,5 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
-
 
 class HrPayslipEmployees(models.TransientModel):
     _name = 'hr.payslip.employees'
@@ -10,23 +8,37 @@ class HrPayslipEmployees(models.TransientModel):
     employee_ids = fields.Many2many('hr.employee', 'hr_employee_group_rel', 'payslip_id', 'employee_id', 'Employees')
 
     def compute_sheet(self):
-        payslips = self.env['hr.payslip']
         [data] = self.read()
         active_id = self.env.context.get('active_id')
-        if active_id:
-            [run_data] = self.env['hr.payslip.run'].browse(active_id).read(['date_start', 'date_end', 'credit_note'])
-        from_date = run_data.get('date_start')
-        to_date = run_data.get('date_end')
         if not data['employee_ids']:
             raise UserError(_("You must select employee(s) to generate payslip(s)."))
+        if active_id:
+            run = self.env['hr.payslip.run'].browse(active_id)
+            from_date = run.date_start
+            to_date = run.date_end
+        else:
+            raise UserError(_("No active payslip run found."))
+
         for employee in self.env['hr.employee'].browse(data['employee_ids']):
-            payslip = self.env['hr.payslip'].create({
+            # Use employee's contract
+            contract = employee.contract_id
+            if not contract:
+                raise UserError(_("Employee %s has no contract." % employee.name))
+
+            payslip_vals = {
                 'employee_id': employee.id,
+                'contract_id': contract.id,
+                'struct_id': contract.struct_id.id,
                 'date_from': from_date,
                 'date_to': to_date,
                 'payslip_run_id': active_id,
-            })
+                'company_id': employee.company_id.id,
+            }
+
+            payslip = self.env['hr.payslip'].create(payslip_vals)
+
             payslip.onchange_employee_id()
+
             payslip.compute_sheet()
-        payslips.compute_sheet()
+
         return {'type': 'ir.actions.act_window_close'}
