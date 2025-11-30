a46407f018fbfbe23633517b646a502f03437c99
ezzatMohsen <ezzatmohsen000@gmail.com>
2025-11-30 12:17:34 +0200

    fix hr payroll employee

--- om_hr_payroll/wizard/hr_payroll_payslips_by_employees.py
+++ om_hr_payroll/wizard/hr_payroll_payslips_by_employees.py
@@ -5,22 +5,25 @@ class HrPayslipEmployees(models.TransientModel):
     _name = 'hr.payslip.employees'
     _description = 'Generate payslips for all selected employees'
 
-    employee_ids = fields.Many2many('hr.employee', 'hr_employee_group_rel', 'payslip_id', 'employee_id', 'Employees')
+    employee_ids = fields.Many2many(
+        'hr.employee', 'hr_employee_group_rel', 'payslip_id', 'employee_id', 'Employees'
+    )
 
     def compute_sheet(self):
         [data] = self.read()
         active_id = self.env.context.get('active_id')
         if not data['employee_ids']:
             raise UserError(_("You must select employee(s) to generate payslip(s)."))
-        if active_id:
-            run = self.env['hr.payslip.run'].browse(active_id)
-            from_date = run.date_start
-            to_date = run.date_end
-        else:
+        if not active_id:
             raise UserError(_("No active payslip run found."))
 
+        run = self.env['hr.payslip.run'].browse(active_id)
+        from_date = run.date_start
+        to_date = run.date_end
+
+        payslips = self.env['hr.payslip']
+
         for employee in self.env['hr.employee'].browse(data['employee_ids']):
-            # Use employee's contract
             contract = employee.contract_id
             if not contract:
                 raise UserError(_("Employee %s has no contract." % employee.name))
@@ -36,9 +39,8 @@ class HrPayslipEmployees(models.TransientModel):
             }
 
             payslip = self.env['hr.payslip'].create(payslip_vals)
+            payslips |= payslip
 
-            payslip.onchange_employee_id()
-
-            payslip.compute_sheet()
+        payslips.compute_sheet()
 
         return {'type': 'ir.actions.act_window_close'}
