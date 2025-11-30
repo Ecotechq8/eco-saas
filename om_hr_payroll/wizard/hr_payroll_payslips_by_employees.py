bab66ce01d58d8afe78aa6a4e4c51750c95f4ff6
ezzatMohsen <ezzatmohsen000@gmail.com>
2025-11-30 12:12:28 +0200

    fix hr payroll employee

--- om_hr_payroll/wizard/hr_payroll_payslips_by_employees.py
+++ om_hr_payroll/wizard/hr_payroll_payslips_by_employees.py
@@ -20,22 +20,13 @@ class HrPayslipEmployees(models.TransientModel):
         if not data['employee_ids']:
             raise UserError(_("You must select employee(s) to generate payslip(s)."))
         for employee in self.env['hr.employee'].browse(data['employee_ids']):
-            slip_data = self.env['hr.payslip'].onchange_employee_id(
-                from_date, to_date, employee.id
-            )
-            res = {
+            payslip = self.env['hr.payslip'].create({
                 'employee_id': employee.id,
-                'name': slip_data['value'].get('name'),
-                'struct_id': slip_data['value'].get('struct_id'),
-                'contract_id': slip_data['value'].get('contract_id'),
-                'payslip_run_id': active_id,
-                'input_line_ids': [(0, 0, x) for x in slip_data['value'].get('input_line_ids')],
-                'worked_days_line_ids': [(0, 0, x) for x in slip_data['value'].get('worked_days_line_ids')],
                 'date_from': from_date,
                 'date_to': to_date,
-                'credit_note': run_data.get('credit_note'),
-                'company_id': employee.company_id.id,
-            }
-            payslips += self.env['hr.payslip'].create(res)
+                'payslip_run_id': active_id,
+            })
+            payslip.onchange_employee_id()
+            payslip.compute_sheet()
         payslips.compute_sheet()
         return {'type': 'ir.actions.act_window_close'}
