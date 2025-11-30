54745c07994018ea5f68c69f656564f6cb7f2b41
ezzatMohsen <ezzatmohsen000@gmail.com>
2025-11-30 12:29:01 +0200

    fix hr payroll employee

--- om_hr_payroll/wizard/hr_payroll_payslips_by_employees.py
+++ om_hr_payroll/wizard/hr_payroll_payslips_by_employees.py
@@ -1,12 +1,15 @@
+# -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
+
 
 class HrPayslipEmployees(models.TransientModel):
     _name = 'hr.payslip.employees'
     _description = 'Generate payslips for all selected employees'
 
     employee_ids = fields.Many2many(
-        'hr.employee', 'hr_employee_group_rel', 'payslip_id', 'employee_id', 'Employees'
+        'hr.employee', 'hr_employee_group_rel',
+        'payslip_id', 'employee_id', 'Employees'
     )
 
     def compute_sheet(self):
@@ -14,21 +17,22 @@ class HrPayslipEmployees(models.TransientModel):
         active_id = self.env.context.get('active_id')
         if not data['employee_ids']:
             raise UserError(_("You must select employee(s) to generate payslip(s)."))
+
         if not active_id:
             raise UserError(_("No active payslip run found."))
 
-        run = self.env['hr.payslip.run'].browse(active_id)
-        from_date = run.date_start
-        to_date = run.date_end
+        payslip_run = self.env['hr.payslip.run'].browse(active_id)
+        from_date = payslip_run.date_start
+        to_date = payslip_run.date_end
 
-        payslips = self.env['hr.payslip']
+        payslip_model = self.env['hr.payslip']
 
         for employee in self.env['hr.employee'].browse(data['employee_ids']):
             contract = employee.contract_id
             if not contract:
-                raise UserError(_("Employee %s has no contract." % employee.name))
+                raise UserError(_("Employee %s has no active contract." % employee.name))
 
-            payslip_vals = {
+            payslip = payslip_model.new({
                 'employee_id': employee.id,
                 'contract_id': contract.id,
                 'struct_id': contract.struct_id.id,
@@ -36,11 +40,17 @@ class HrPayslipEmployees(models.TransientModel):
                 'date_to': to_date,
                 'payslip_run_id': active_id,
                 'company_id': employee.company_id.id,
-            }
+            })
 
-            payslip = self.env['hr.payslip'].create(payslip_vals)
-            payslips |= payslip
+            payslip.onchange_employee_id()
 
-        payslips.compute_sheet()
+            for line in payslip.worked_days_line_ids:
+                line.contract_id = contract.id
+            for line in payslip.input_line_ids:
+                line.contract_id = contract.id
+
+            payslip.compute_sheet()
+
+            payslip_model.create(payslip._convert_to_write(payslip._cache))
 
         return {'type': 'ir.actions.act_window_close'}
