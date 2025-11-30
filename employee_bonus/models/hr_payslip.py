9e39a03951ec7bbd2405418e29b02dd4557f6863
ezzatMohsen <ezzatmohsen000@gmail.com>
2025-11-30 11:00:41 +0200

    fix hr payroll employee

--- employee_bonus/models/hr_payslip.py
+++ employee_bonus/models/hr_payslip.py
@@ -4,43 +4,44 @@ from odoo import fields, models, api, _
 class HrPayslip(models.Model):
     _inherit = 'hr.payslip'
 
-    bonus_line_ids = fields.One2many(comodel_name='employee.bonus', inverse_name='payslip_id')
+    penalty_line_ids = fields.One2many(comodel_name='penalty.request', inverse_name='payslip_id')
 
     def compute_sheet(self):
-        # Compute your input lines including bonuses before computing the sheet
-        self._compute_bonus_input_lines()
-        res = super(HrPayslip, self).compute_sheet()
-        return res
+        self._compute_penalty_input_lines()
+        return super(HrPayslip, self).compute_sheet()
 
-    def _compute_bonus_input_lines(self):
+    def _compute_penalty_input_lines(self):
         for item in self:
             if item.employee_id:
-                bonus_ids = self.env['employee.bonus'].search([
+                penalty_ids = self.env['penalty.request.payment'].search([
                     ('employee_id', '=', item.employee_id.id),
-                    ('state', '=', 'approved'),
+                    ('penalty_id.state', '=', 'approved'),
                     ('date', '>=', item.date_from),
                     ('date', '<=', item.date_to)
                 ])
 
-                bonus_needs_to_be_deleted = item.input_line_ids.filtered(
-                    lambda x: x.is_bonus and not x.bonus_id)
-                bonus_needs_to_be_deleted.unlink()
+                penalty_needs_to_be_deleted = item.input_line_ids.filtered(
+                    lambda x: x.is_penalty
+                )
+                penalty_needs_to_be_deleted.unlink()
 
-                for line in bonus_ids:
-                    input_line = item.input_line_ids.filtered(lambda x: x.bonus_id.id == line.id)
+                for line in penalty_ids:
+                    input_line = item.input_line_ids.filtered(
+                        lambda x: x.penalty_payment_id.id == line.id
+                    )
                     if input_line:
                         input_line.write({
-                            'input_type_id': line.bonus_type.input_type_id.id,
+                            'input_type_id': line.penalty_id.input_type_id.id,
                             'name': line.name,
                             'amount': line.amount,
-                            'bonus_id': line.id,
-                            'is_bonus': True
+                            'penalty_payment_id': line.id,
+                            'is_penalty': True
                         })
                     else:
                         item.input_line_ids = [(0, 0, {
-                            'input_type_id': line.bonus_type.input_type_id.id,
+                            'input_type_id': line.penalty_id.input_type_id.id,
                             'name': line.name,
                             'amount': line.amount,
-                            'bonus_id': line.id,
-                            'is_bonus': True
+                            'penalty_payment_id': line.id,
+                            'is_penalty': True
                         })]
