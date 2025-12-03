a9d5797bc019cd9b147c692a3dcedd38e117329f
ezzatMohsen <ezzatmohsen000@gmail.com>
2025-12-03 13:43:54 +0200

    edit theme

--- employee_bonus/models/hr_payslip.py
+++ employee_bonus/models/hr_payslip.py
@@ -4,44 +4,44 @@ from odoo import fields, models, api, _
 class HrPayslip(models.Model):
     _inherit = 'hr.payslip'
 
-    penalty_line_ids = fields.One2many(comodel_name='penalty.request', inverse_name='payslip_id')
+    bonus_line_ids = fields.One2many(comodel_name='employee.bonus', inverse_name='payslip_id')
 
     def compute_sheet(self):
-        self._compute_penalty_input_lines()
-        return super(HrPayslip, self).compute_sheet()
+        self._compute_input_line_ids()
+        res = super(HrPayslip, self).compute_sheet()
+        return res
 
-    def _compute_penalty_input_lines(self):
+    @api.depends('employee_id', 'contract_id', 'struct_id', 'date_from', 'date_to', 'struct_id')
+    def _compute_input_line_ids(self):
+        res = super(HrPayslip, self)._compute_input_line_ids()
         for item in self:
             if item.employee_id:
-                penalty_ids = self.env['penalty.request.payment'].search([
-                    ('employee_id', '=', item.employee_id.id),
-                    ('penalty_id.state', '=', 'approved'),
-                    ('date', '>=', item.date_from),
-                    ('date', '<=', item.date_to)
-                ])
+                bonus_ids = self.env['employee.bonus'].search([
+                    ('employee_id', '=', item.employee_id.id), ('state', '=', 'approved'),
+                    ('date', '>=', item.date_from), ('date', '<=', item.date_to)])
 
-                penalty_needs_to_be_deleted = item.input_line_ids.filtered(
-                    lambda x: x.is_penalty
-                )
-                penalty_needs_to_be_deleted.unlink()
-
-                for line in penalty_ids:
-                    input_line = item.input_line_ids.filtered(
-                        lambda x: x.penalty_payment_id.id == line.id
-                    )
-                    if input_line:
-                        input_line.write({
-                            'input_type_id': line.penalty_id.input_type_id.id,
-                            'name': line.name,
-                            'amount': line.amount,
-                            'penalty_payment_id': line.id,
-                            'is_penalty': True
-                        })
-                    else:
-                        item.input_line_ids = [(0, 0, {
-                            'input_type_id': line.penalty_id.input_type_id.id,
-                            'name': line.name,
-                            'amount': line.amount,
-                            'penalty_payment_id': line.id,
-                            'is_penalty': True
-                        })]
+                bonus_needs_to_be_deleted = item.input_line_ids.filtered(
+                    lambda x: x.is_bonus == True and not x.bonus_id.id)
+                bonus_needs_to_be_deleted.unlink()
+                if bonus_ids:
+                    for line in bonus_ids:
+                        if (line.bonus_type.input_type_id and
+                                not line.bonus_type.input_type_id.id in item.input_line_ids.mapped(
+                                    "input_type_id").ids):
+                            item.input_line_ids = [(0, 0, {
+                                'input_type_id': line.bonus_type.input_type_id.id,
+                                'name': line.name,
+                                'amount': line.amount,
+                                'bonus_id': line.id,
+                                'is_bonus': True
+                            })]
+                        else:
+                            input_line = item.input_line_ids.filtered(lambda x: x.bonus_id.id == line.id)
+                            input_line.write({
+                                'input_type_id': line.bonus_type.input_type_id.id,
+                                'name': line.name,
+                                'amount': line.amount,
+                                'bonus_id': line.id,
+                                'is_bonus': True
+                            })
+        return res
