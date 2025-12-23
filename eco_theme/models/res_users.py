8966361014b0ae961d7c5c8f47413daefe5bd294
ahmedefad <a.ismail@ecotech-efad.com>
2025-12-23 14:56:23 +0200

    feat: default icon

--- eco_theme/models/res_users.py
+++ eco_theme/models/res_users.py
@@ -8,22 +8,25 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+
 class Users(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
     def _init_odoobot(self):
         self.ensure_one()
 
-        odoobot_id = self.env['ir.model.data']._xmlid_to_res_id("base.partner_root")
-        channel = self.env['discuss.channel'].channel_get(
+        odoobot_id = self.env["ir.model.data"]._xmlid_to_res_id("base.partner_root")
+        channel = self.env["discuss.channel"].channel_get(
             [odoobot_id, self.partner_id.id]
         )
 
         message = Markup(
-            "%s<br/>%s<br/><b>%s</b> <span class=\"o_odoobot_command\">:)</span>"
+            '%s<br/>%s<br/><b>%s</b> <span class="o_odoobot_command">:)</span>'
         ) % (
             _("Hello,"),
-            _("Eco Pro's chat helps employees collaborate efficiently. I'm here to help you discover its features."),
+            _(
+                "Eco Pro's chat helps employees collaborate efficiently. I'm here to help you discover its features."
+            ),
             _("Try to send me an emoji"),
         )
 
@@ -35,28 +38,33 @@ class Users(models.Model):
             subtype_xmlid="mail.mt_comment",
         )
 
-        self.sudo().odoobot_state = 'onboarding_emoji'
+        self.sudo().odoobot_state = "onboarding_emoji"
         return channel
-    
 
     @api.model
     def _get_activity_groups(self):
         activities = super()._get_activity_groups()
         for activity in activities:
-            module_name = activity.get('model','')
-            Model = self.env[module_name]
-            original_module = Model._original_module
-            eco_modules = ['eco_theme','eco_advanced']
-            for eco_module in eco_modules:
-                icon_file = f'{eco_module}/static/src/img/icons/{original_module}.png'
-                try:
-                    path_exist = file_path(icon_file)
-                except Exception:
-                    path_exist = False
-                icon_path = "/"+icon_file
-                _logger.info(f"Checking icon path: {path_exist}")
-                if path_exist:
-                    activity['icon'] = icon_path
+            custom_icon = self._find_custom_icon(activity)
+            if custom_icon:
+                activity["icon"] = custom_icon
+            else:
+                activity["icon"] = ""
         return activities
 
-       
+    def _find_custom_icon(self, activity):
+        module_name = activity.get("model", "")
+        Model = self.env[module_name]
+        original_module = Model._original_module
+        eco_modules = ["eco_theme", "eco_advanced"]
+        icon_path = False
+        for eco_module in eco_modules:
+            icon_file = f"{eco_module}/static/src/img/icons/{original_module}.png"
+            try:
+                path_exist = file_path(icon_file)
+                icon_path = "/" + icon_file
+                _logger.info(f"Checking icon path: {path_exist}")
+                break
+            except Exception:
+                continue
+        return icon_path
