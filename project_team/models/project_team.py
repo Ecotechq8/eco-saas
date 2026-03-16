941aaf3bcf471ee22d32e3a1d58eb6f55f14b441
ezzatMohsen <ezzatmohsen000@gmail.com>
2026-03-16 22:44:32 +0200

    team

--- project_team/models/project_team.py
+++ project_team/models/project_team.py
@@ -6,14 +6,19 @@ from odoo import api, fields, models
 class ProjectProject(models.Model):
     _inherit = 'project.project'
 
-    members_ids = fields.Many2many('res.users', 'project_user_rel', 'project_id',
-                                   'user_id', 'Project Members', help="""Project's
-                               members are users who can have an access to
-                               the tasks related to this project."""
-                                   )
+    members_ids = fields.Many2many(
+        'hr.employee',
+        'project_employee_rel',
+        'project_id',
+        'employee_id',
+        string='Project Members'
+    )
     team_id = fields.Many2one('crm.team', "Project Team",
                               domain=[('type_team', '=', 'project')])
 
     @api.onchange('team_id')
     def _get_team_members(self):
-        self.update({"members_ids": [(6, 0, self.team_id.team_members_ids.ids)]})
+        employees = self.env['hr.employee'].search([
+            ('user_id', 'in', self.team_id.team_members_ids.ids)
+        ])
+        self.members_ids = [(6, 0, employees.ids)]
