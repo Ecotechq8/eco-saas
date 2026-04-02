c6104ffd89b3a69c53b15bed1599d0121e5a945f
ezzatMohsen <ezzatmohsen000@gmail.com>
2026-04-02 16:36:06 +0200

    web url

--- eco_construction/models/res_company.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, api
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    @api.model
-    def _update_tax_rounding_globally(self):
-        companies = self.search([])
-        companies.write({'tax_calculation_rounding_method': 'round_globally'})
