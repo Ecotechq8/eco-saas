11df860a296399ce43e3edfe099e250e3c9a3740
Ahmed  Emara <ahmedemara@MacBook-Pro.local>
2026-04-20 12:06:06 +0200

    Fix upgrade addons

--- verts_v15_print_template/__manifest__.py
+++ verts_v15_print_template/__manifest__.py
@@ -12,7 +12,7 @@
     'summary': 'Prinouts & Reports Related Configurations',
     'data': [
         ##'views/web_widget_color_view.xml',
-        'views/res_company_view.xml',
+        # 'views/res_company_view.xml',
     ],
     'images': [],
     'installable': True,
