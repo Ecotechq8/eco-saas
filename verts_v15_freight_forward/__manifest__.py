36813f15c463fa3616564e4c39c48c3a609e7dd3
Ahmed  Emara <ahmedemara@MacBook-Pro.local>
2026-05-25 10:59:24 +0300

    MOD verts_v15_freight_forward

--- verts_v15_freight_forward/__manifest__.py
+++ verts_v15_freight_forward/__manifest__.py
@@ -9,7 +9,7 @@
     "description": """This module adds the features of finance localization related to India""",
     "website": "http://www.verts.co.in",
     "depends": ['sale', 'stock', 'sale_management', 'purchase', 'verts_v15_basic_masters', 'verts_v15_print_template',
-                'product', 'crm', 'analytic'
+                'product', 'crm', 'analytic', 'verts_v15_profiles'
                 ],
     "category": "Generic Modules",
     'license': "LGPL-3",
