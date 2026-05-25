c8a1ad83dafab7eaa12fd290d32ea95f5397bb2f
Ahmed  Emara <ahmedemara@MacBook-Pro.local>
2026-05-25 11:24:57 +0300

    MOD verts_v15_freight_forward

--- verts_v15_profiles/__manifest__.py
+++ verts_v15_profiles/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Access Control",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "author": "VERTS Services India Pvt. Ltd.",
     "website": 'http://www.verts.co.in',
     "license": 'AGPL-3',
