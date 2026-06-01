948102e945f544f4235acd2bb5a9b687aa3a151b
ezzatMohsen <ezzatmohsen000@gmail.com>
2026-06-01 13:35:09 +0300

    locations

--- location_restriction_for_user/models/res_user.py
+++ location_restriction_for_user/models/res_user.py
@@ -1,8 +1,24 @@
-from odoo import fields, api, models
+from odoo import fields, models
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
     allow_location_ids = fields.Many2many('stock.location')
+    inventory_dashboard_warehouse_ids = fields.Many2many(
+        'stock.warehouse',
+        'res_users_inventory_dashboard_warehouse_rel',
+        'user_id',
+        'warehouse_id',
+        string='Inventory Dashboard Warehouses',
+        check_company=True,
+        help='Warehouses visible on the Inventory Dashboard. If empty, the Default Warehouse is used.',
+    )
 
+    @property
+    def SELF_READABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + ['inventory_dashboard_warehouse_ids']
+
+    @property
+    def SELF_WRITEABLE_FIELDS(self):
+        return super().SELF_WRITEABLE_FIELDS + ['inventory_dashboard_warehouse_ids']
--- location_restriction_for_user/models/stock_picking_type.py
+++ location_restriction_for_user/models/stock_picking_type.py
@@ -5,8 +5,12 @@ class StockPickingType(models.Model):
     _inherit = "stock.picking.type"
 
     @api.model
-    def _get_user_selected_default_warehouse(self):
+    def _get_user_inventory_dashboard_warehouses(self):
         user = self.env.user.with_company(self.env.company)
+        if "inventory_dashboard_warehouse_ids" in user._fields:
+            warehouses = user.inventory_dashboard_warehouse_ids
+            if warehouses:
+                return warehouses
         for field_name in ("property_warehouse_id", "default_warehouse_id"):
             if field_name in user._fields:
                 warehouse = user[field_name]
@@ -15,20 +19,20 @@ class StockPickingType(models.Model):
         return self.env["stock.warehouse"]
 
     @api.model
-    def _restrict_dashboard_domain_to_default_warehouse(self, domain):
-        if not self.env.context.get("restrict_inventory_dashboard_default_warehouse"):
+    def _restrict_dashboard_domain_to_user_warehouses(self, domain):
+        if not self.env.context.get("restrict_inventory_dashboard_warehouses"):
             return domain
 
-        warehouse = self._get_user_selected_default_warehouse()
-        if not warehouse:
+        warehouses = self._get_user_inventory_dashboard_warehouses()
+        if not warehouses:
             return domain
 
-        return [("warehouse_id", "=", warehouse.id)] + list(domain or [])
+        return [("warehouse_id", "in", warehouses.ids)] + list(domain or [])
 
     @api.model
     def _search(self, domain, *args, **kwargs):
         return super()._search(
-            self._restrict_dashboard_domain_to_default_warehouse(domain),
+            self._restrict_dashboard_domain_to_user_warehouses(domain),
             *args,
             **kwargs
         )
@@ -36,7 +40,7 @@ class StockPickingType(models.Model):
     @api.model
     def read_group(self, domain, fields, groupby, *args, **kwargs):
         return super().read_group(
-            self._restrict_dashboard_domain_to_default_warehouse(domain),
+            self._restrict_dashboard_domain_to_user_warehouses(domain),
             fields,
             groupby,
             *args,
